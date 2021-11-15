57d1dccf6f1cce9e0356c54e11be001a9435eef1
Mateusz Wilk <matewilk@gmail.com>
2021-11-15 19:35:15 +0000

    (feat): parametrise dybamodb table docker image build

--- docker/dynamodb/create_admin_user.sh
+++ docker/dynamodb/create_admin_user.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+DYNAMODB_ENDPOINT=$1 # for example: http://dynamodb:8000
+DYNAMODB_REGION=$2 # for example: local or eu-west-2
+
 aws dynamodb put-item \
     --table-name Questo \
     --item file://admin_record.json \
-    --endpoint-url http://dynamodb:8000 \
-    --region eu-west-2
+    --endpoint-url "$DYNAMODB_ENDPOINT" \
+    --region "$DYNAMODB_REGION"
--- docker/dynamodb/create_dynamodb_local.sh
+++ docker/dynamodb/create_dynamodb_local.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+DYNAMODB_ENDPOINT=$1 # for example: http://dynamodb:8000
+DYNAMODB_REGION=$2 # for example: local or eu-west-2
+
 aws dynamodb create-table \
     --cli-input-json file://questo_db_definition.json \
-    --endpoint-url http://dynamodb:8000 \
-    --region eu-west-2
+    --endpoint-url "$DYNAMODB_ENDPOINT" \
+    --region "$DYNAMODB_REGION"
