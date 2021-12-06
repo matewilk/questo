a7d0377e78de1544bb62d6d88376643fc75c29c1
Mateusz Wilk <matewilk@gmail.com>
2021-12-06 22:39:42 +0000

    (feat): add group.name to ingress alb

--- tf/app/main.tf
+++ tf/app/main.tf
@@ -97,8 +97,10 @@ resource "kubernetes_ingress" "questo-server-ingress" {
     name      = "questo-server-ingress-${var.env}"
     namespace = kubernetes_namespace.app-namespace.metadata.0.name
     annotations = {
-      "alb.ingress.kubernetes.io/load-balancer-name" = "questo-server-alb-${var.env}"
       "kubernetes.io/ingress.class"                  = "alb"
+      "alb.ingress.kubernetes.io/load-balancer-name" = "questo-alb-${var.env}"
+      "alb.ingress.kubernetes.io/group.name"         = "questo-${var.env}"
+      "alb.ingress.kubernetes.io/group.order"        = "999"
       "alb.ingress.kubernetes.io/target-type"        = "ip"
       "alb.ingress.kubernetes.io/scheme"             = "internet-facing"
       "alb.ingress.kubernetes.io/listen-ports"       = "[{\"HTTP\": 4000}]"
