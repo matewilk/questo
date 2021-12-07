21d16a12c670d81a7914b6358b77f71aae232eff
Mateusz Wilk <matewilk@gmail.com>
2021-12-07 01:51:29 +0000

    (feat): change listener path to /*

--- tf/app/main.tf
+++ tf/app/main.tf
@@ -113,7 +113,7 @@ resource "kubernetes_ingress" "questo-server-ingress" {
     rule {
       http {
         path {
-          path = "/api"
+          path = "/*"
           backend {
             service_name = kubernetes_service.questo-server-service.metadata.0.name
             service_port = 4000
