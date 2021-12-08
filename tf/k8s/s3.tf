2a27daf1d3ed9a1442cba2a0c86ca48ffbd526d4
Mateusz Wilk <matewilk@gmail.com>
2021-12-08 22:43:43 +0000

    (feat): working nri-bundle on t2.large instance type

--- tf/k8s/s3.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_s3_bucket_object" "kube_config" {
-  bucket       = var.questo-infra-bucket
-  key          = "${var.env}/eks/kubeconfig"
-  content      = module.eks.kubeconfig
-  content_type = "text/*"
-}
