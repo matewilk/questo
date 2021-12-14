baf3bd65a460381a2391da2ffab15d6352f58fd2
Mateusz Wilk <matewilk@gmail.com>
2021-12-14 00:03:00 +0000

    (feat): fixed redis setup without passwd

--- tf/k8s/helm-redis.tf
+++ tf/k8s/helm-redis.tf
@@ -1,9 +1,27 @@
+variable "extraEnvVars" {
+  description = "extra redis env vars"
+  type = list(object({
+    name  = any
+    value = string
+  }))
+  default = [{
+    name : "ALLOW_EMPTY_PASSWORD",
+    value : "yes"
+  }]
+}
+
 resource "helm_release" "redis-chart" {
   name = "redis-${var.env}"
 
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "redis"
-  namespace  = "kube-system"
+  namespace  = "redis"
+  create_namespace = true
+
+  set {
+    name  = "auth.enabled"
+    value = "false"
+  }
 
   set {
     name  = "master.containerPort"
@@ -12,7 +30,12 @@ resource "helm_release" "redis-chart" {
 
   set {
     name  = "master.extraEnvVars"
-    value = { "ALLOW_EMPTY_PASSWORD" = "true" }
+    value = yamlencode(var.extraEnvVars)
+  }
+
+  set {
+    name  = "replica.extraEnvVars"
+    value = yamlencode(var.extraEnvVars)
   }
 
   set {
