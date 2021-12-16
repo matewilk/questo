6e2390446c2ba8950865e28becff9b1d2c24dab0
Mateusz Wilk <matewilk@gmail.com>
2021-12-16 15:38:57 +0000

    (chore): destroy route53 cert resources

--- tf/app/main.tf
+++ tf/app/main.tf
@@ -105,10 +105,11 @@ resource "kubernetes_ingress" "questo-server-ingress" {
       "alb.ingress.kubernetes.io/group.order"        = "999"
       "alb.ingress.kubernetes.io/target-type"        = "ip"
       "alb.ingress.kubernetes.io/scheme"             = "internet-facing"
-      "alb.ingress.kubernetes.io/listen-ports"       = "[{\"HTTPS\": 443}, {\"HTTP\": 4000}]"
+#      "alb.ingress.kubernetes.io/listen-ports"       = "[{\"HTTPS\": 443}, {\"HTTP\": 4000}]"
+      "alb.ingress.kubernetes.io/listen-ports"       = "[{\"HTTP\": 4000}]"
       "alb.ingress.kubernetes.io/healthcheck-path"   = "/health"
       "alb.ingress.kubernetes.io/healthcheck-port"   = "traffic-port"
-      "alb.ingress.kubernetes.io/certificate-arn"    = aws_acm_certificate_validation.questo.certificate_arn
+#      "alb.ingress.kubernetes.io/certificate-arn"    = aws_acm_certificate_validation.questo.certificate_arn
     }
   }
 
