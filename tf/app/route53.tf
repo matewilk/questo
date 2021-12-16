e4000288c109e90dc7bbe86396d156b6981b09dc
Mateusz Wilk <matewilk@gmail.com>
2021-12-16 19:15:43 +0000

    (feat): update route53 record

--- tf/app/route53.tf
+++ tf/app/route53.tf
@@ -40,7 +40,7 @@ resource "aws_route53_record" "questo" {
 
 resource "aws_route53_record" "alb-routing" {
   name    = "${var.env}.questo.live"
-  records = [data.aws_alb.questo-alb.dns_name]
+  records = ["${data.aws_alb.questo-alb.ip_address_type}.${data.aws_alb.questo-alb.dns_name}"]
   ttl     = 300
   type    = "A"
   zone_id = data.aws_route53_zone.questo.zone_id
